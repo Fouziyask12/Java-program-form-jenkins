5c27bd55df0544d0dd4fa66029443d12688c59f0
Fouziyask12 <128252049+Fouziyask12@users.noreply.github.com>
2024-03-22 00:36:54 +0530

    second

--- Main.java
+++ Main.java
@@ -7,5 +7,6 @@ public class Main{
         System.out.println("Output from java program");
         System.out.println(new Date());
         System.out.println("input of the code");
+        System.out.println("This is complete code ");
     }
 }
